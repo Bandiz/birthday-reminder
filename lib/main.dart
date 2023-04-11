caa4e71a1dd6e671adc39e13ab08ae84ff15b2cc
Linas Jakseboga <linas.jakseboga@gmail.com>
2023-04-11 21:37:54 +0200

    added simple notifications on button press

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+// void onDidReceiveNotificationResponse(
+//     NotificationResponse notificationResponse) async {
+//   final String? payload = notificationResponse.payload;
+//   if (notificationResponse.payload != null) {
+//     debugPrint('notification payload: $payload');
+//   }
+//   await Navigator.push(
+//     context,
+//     MaterialPageRoute<void>(builder: (context) => SecondScreen(payload)),
+//   );
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings();
+  const InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsDarwin);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(const MyApp());
 }
 
@@ -22,7 +47,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -48,6 +73,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -63,6 +91,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 2.0));
+    const double defaultPadding = 5.0;
+
+    Future<void> showMyDialog(String message) async {
+      const AndroidNotificationDetails androidPlatformChannelSpecifics =
+          AndroidNotificationDetails('your channel id', 'your channel name',
+              channelDescription: 'your channel description',
+              importance: Importance.max,
+              priority: Priority.high,
+              ticker: 'ticker');
+      const NotificationDetails platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics);
+
+      await flutterLocalNotificationsPlugin.show(0, 'New Notification',
+          'You have a new notification!', platformChannelSpecifics,
+          payload: message);
+
+      // return showDialog(
+      //     context: context,
+      //     builder: ((BuildContext context) => AlertDialog(
+      //           title: const Text('Day of week'),
+      //           content: Text(message),
+      //           actions: <Widget>[
+      //             TextButton(
+      //                 onPressed: () => Navigator.pop(context, 'OK'),
+      //                 child: const Text('OK'))
+      //           ],
+      //         )));
+    }
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -75,35 +135,74 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: GridView.count(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        crossAxisCount: 7,
+        crossAxisSpacing: defaultPadding,
+        padding: const EdgeInsets.only(top: defaultPadding),
+        children: <Widget>[
+          ElevatedButton(
+              style: style,
+              child: const Text('Mon'),
+              onPressed: () => showMyDialog('Monday')),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Tuesday'),
+            child: const Text('Tue'),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Wednesday'),
+            child: const Text('Wed'),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Thursday'),
+            child: const Text('Thur'),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Friday'),
+            child: const Text('Fri'),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Saturday'),
+            child: const Text('Sat'),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () => showMyDialog('Sunday'),
+            child: const Text('Sun'),
+          )
+        ],
+        // Column(
+        //   // Column is also a layout widget. It takes a list of children and
+        //   // arranges them vertically. By default, it sizes itself to fit its
+        //   // children horizontally, and tries to be as tall as its parent.
+        //   //
+        //   // Invoke "debug painting" (press "p" in the console, choose the
+        //   // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        //   // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        //   // to see the wireframe for each widget.
+        //   //
+        //   // Column has various properties to control how it sizes itself and
+        //   // how it positions its children. Here we use mainAxisAlignment to
+        //   // center the children vertically; the main axis here is the vertical
+        //   // axis because Columns are vertical (the cross axis would be
+        //   // horizontal).
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     const Text(
+        //       'You have clicked the button this many times:',
+        //     ),
+        //     Text(
+        //       '$_counter',
+        //       style: Theme.of(context).textTheme.headlineMedium,
+        //     ),
+        //   ],
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
