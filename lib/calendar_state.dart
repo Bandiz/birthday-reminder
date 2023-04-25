3d4b0ad5fbfa2afd6abc0f56d531ffb6398fc282
Linas Jakseboga <linas.jakseboga@gmail.com>
2023-04-25 22:11:35 +0200

    added event crud operations

--- lib/calendar_state.dart
+++ lib/calendar_state.dart
@@ -10,14 +10,21 @@ class CalendarState extends ChangeNotifier {
     _dateEvents = kEvents;
   }
 
-  List<Event> getEvents(DateTime date) => _dateEvents[date] ?? [];
+  List<Event> getEvents(DateTime date) =>
+      List.unmodifiable(_dateEvents[date] ?? []);
 
   void toggleEvent(DateTime date, int index) {
     final events = _dateEvents[date];
 
     if (events == null) return;
 
-    events[index].isSelected = !events[index].isSelected;
+    final event = events[index];
+
+    for (var element in events.where((x) => x.isSelected && x != event)) {
+      element.isSelected = false;
+    }
+
+    event.isSelected = !event.isSelected;
     notifyListeners();
   }
 
@@ -26,6 +33,19 @@ class CalendarState extends ChangeNotifier {
 
     events.add(Event(title: title, date: date));
     _dateEvents[date] = events;
+    notifyListeners();
+  }
+
+  void removeEvent(DateTime date, Event event) {
+    final events = _dateEvents[date] ?? [];
+    events.remove(event);
+    notifyListeners();
+  }
+
+  void updateEvent(DateTime date, Event event, String title) {
+    final events = _dateEvents[date] ?? [];
+    final index = events.indexOf(event);
+    events.replaceRange(index, index + 1, [Event(date: date, title: title)]);
     notifyListeners();
   }
 }
--- lib/day_events.dart
+++ lib/day_events.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import 'calendar_state.dart';
+import 'utils.dart';
 
 class DayEvents extends StatelessWidget {
   final DateTime currentDate;
@@ -13,69 +15,128 @@ class DayEvents extends StatelessWidget {
   Widget build(BuildContext context) {
     final calendarState = Provider.of<CalendarState>(context, listen: true);
     final events = calendarState.getEvents(currentDate);
+    final isSelected = events.any((x) => (x.isSelected));
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-            'Selected Date: ${DateFormat("yyyy-MM-dd").format(currentDate)}'),
-      ),
-      body: Center(
-        child: Expanded(
-          child: ListView.builder(
-            itemCount: events.length,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 12.0,
-                  vertical: 4.0,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: ListTile(
-                  onTap: () => calendarState.toggleEvent(currentDate, index),
-                  title: Text('${events[index]}'),
-                  selected: events[index].isSelected,
+        appBar: AppBar(
+          title: Text(
+              'Selected Date: ${DateFormat("yyyy-MM-dd").format(currentDate)}'),
+        ),
+        body: Center(
+          child: Expanded(
+            child: ListView.builder(
+              itemCount: events.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 12.0,
+                    vertical: 4.0,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: ListTile(
+                    onTap: () async {
+                      final controller =
+                          TextEditingController(text: events[index].title);
+                      await showDialog<void>(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text("Edit event"),
+                          content: TextField(
+                            controller: controller,
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red[900],
+                                  foregroundColor: Colors.white),
+                              onPressed: () {
+                                calendarState.removeEvent(
+                                    currentDate, events[index]);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Delete'),
+                            ),
+                            TextButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              child: const Text('Save'),
+                              onPressed: () {
+                                if (controller.text.isEmpty) {
+                                  return;
+                                }
+                                calendarState.updateEvent(currentDate,
+                                    events[index], controller.text);
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    title: Text('${events[index]}'),
+                    selected: events[index].isSelected,
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              final controller = TextEditingController();
+              await showDialog<void>(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("Enter new event"),
+                  content: TextField(
+                    controller: controller,
+                  ),
+                  actions: [
+                    TextButton(
+                      child: const Text('Cancel'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('Save'),
+                      onPressed: () {
+                        if (controller.text.isEmpty) {
+                          return;
+                        }
+                        calendarState.addEvent(currentDate, controller.text);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
                 ),
               );
             },
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            final controller = TextEditingController();
-            await showDialog<void>(
-                context: context,
-                builder: (context) => AlertDialog(
-                      title: const Text("Enter new event"),
-                      content: TextField(
-                        controller: controller,
-                      ),
-                      actions: [
-                        TextButton(
-                          child: const Text('Cancel'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        TextButton(
-                          child: const Text('Save'),
-                          onPressed: () {
-                            if (controller.text.isEmpty) {
-                              return;
-                            }
-                            calendarState.addEvent(
-                                currentDate, controller.text);
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    ),
-                barrierDismissible: true);
-          },
-          child: const Icon(Icons.add)),
-    );
+            child: const Icon(Icons.add)));
   }
+}
+
+enum EventAction { edit, delete }
+
+class PopupMenuButtonWidget extends PopupMenuItem {
+  final IconData icon;
+  final EventAction value;
+
+  PopupMenuButtonWidget({super.key, required this.icon, required this.value})
+      : super(
+          value: value,
+          child: Row(
+            children: [
+              Icon(icon),
+              const SizedBox(width: 8.0),
+            ],
+          ),
+        );
 }
