066ff6c55f0f80f3a568e7a3a9f8a1455e341746
Linas Jakseboga <linas.jakseboga@gmail.com>
2023-05-09 15:21:52 +0200

    moved home_page to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 import 'calendar_state.dart';
 import 'models/event.dart';
-import 'pages/new_event_page.dart';
+import 'pages/home_page.dart';
 
 final kToday = DateTime.now().toUtc();
 final kFirstDay = DateTime(kToday.year, 1, 31);
@@ -104,108 +103,5 @@ class BirthdayReminderApp extends StatelessWidget {
       ),
       home: const HomePage(title: 'Home page'),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  final String title;
-
-  const HomePage({super.key, required this.title});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _selectedDay = _focusedDay;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    if (!isSameDay(_selectedDay, selectedDay)) {
-      setState(() {
-        _selectedDay = selectedDay;
-        _focusedDay = focusedDay;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<CalendarState>(builder: (context, state, child) {
-      final events = state.getEvents(_focusedDay);
-      return Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  state.clear();
-                },
-              )
-            ],
-          ),
-          body: Column(
-            children: [
-              TableCalendar<Event>(
-                firstDay: kFirstDay,
-                lastDay: kLastDay,
-                focusedDay: _focusedDay,
-                selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-                headerStyle: const HeaderStyle(
-                    formatButtonVisible: false, titleCentered: true),
-                eventLoader: (date) => state.getEvents(date),
-                startingDayOfWeek: StartingDayOfWeek.monday,
-                calendarStyle: const CalendarStyle(
-                  outsideDaysVisible: true,
-                ),
-                onDaySelected: _onDaySelected,
-                onPageChanged: (focusedDay) {
-                  _focusedDay = focusedDay;
-                },
-              ),
-              Expanded(
-                  child: SizedBox(
-                      height: double.infinity,
-                      width: double.infinity,
-                      child: ListView.builder(
-                          itemCount: events.length,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 2, horizontal: 8),
-                                child: ListTile(
-                                    title: Text(events[index].title),
-                                    shape: RoundedRectangleBorder(
-                                      side: const BorderSide(
-                                          color: Colors.black, width: 1),
-                                      borderRadius: BorderRadius.circular(5),
-                                    )));
-                          })))
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NewEventPage(),
-                  ),
-                );
-              },
-              child: const Icon(Icons.add)));
-    });
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../calendar_state.dart';
+import '../models/event.dart';
+import '../utils.dart';
+import 'new_event_page.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDay = _focusedDay;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CalendarState>(builder: (context, state, child) {
+      final events = state.getEvents(_focusedDay);
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  state.clear();
+                },
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              TableCalendar<Event>(
+                firstDay: kFirstDay,
+                lastDay: kLastDay,
+                focusedDay: _focusedDay,
+                selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                headerStyle: const HeaderStyle(
+                    formatButtonVisible: false, titleCentered: true),
+                eventLoader: (date) => state.getEvents(date),
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                calendarStyle: const CalendarStyle(
+                  outsideDaysVisible: true,
+                ),
+                onDaySelected: _onDaySelected,
+                onPageChanged: (focusedDay) {
+                  _focusedDay = focusedDay;
+                },
+              ),
+              Expanded(
+                  child: SizedBox(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: ListView.builder(
+                          itemCount: events.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 2, horizontal: 8),
+                                child: ListTile(
+                                    title: Text(events[index].title),
+                                    shape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: Colors.black, width: 1),
+                                      borderRadius: BorderRadius.circular(5),
+                                    )));
+                          })))
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const NewEventPage(),
+                  ),
+                );
+              },
+              child: const Icon(Icons.add)));
+    });
+  }
+}
