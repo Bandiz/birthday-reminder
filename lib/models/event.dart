531671c8ec0b2e1ca5a2b098fa57e4bc337717f2
Linas Jakseboga <linas.jakseboga@gmail.com>
2023-05-16 15:46:22 +0200

    updated event model

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -3,16 +3,38 @@ import 'package:hive/hive.dart';
 part 'event.g.dart';
 
 @HiveType(typeId: 1)
-class Event extends HiveObject {
-  @HiveField(0)
-  final int id;
+class EventObject extends HiveObject {
   @HiveField(1)
   final String title;
   @HiveField(2)
   final DateTime date;
 
-  Event({required this.id, required this.title, required this.date});
+  EventObject({required this.title, required this.date});
 
   @override
   String toString() => title;
+}
+
+extension EventObjectExtension on EventObject {
+  Event getEvent(dynamic key) => Event.fromKeyPair(key, this);
+}
+
+class Event {
+  final int id;
+  final String title;
+  final DateTime birthDate;
+
+  Event.fromKeyPair(dynamic key, EventObject object)
+      : id = key as int,
+        title = object.title,
+        birthDate = object.date;
+
+  const Event({required this.id, required this.title, required this.birthDate});
+
+  DateTime getNormalizedDate([int? year]) {
+    year ??= DateTime.now().year;
+    return DateTime.utc(year, birthDate.month, birthDate.day);
+  }
+
+  EventObject getEventObject() => EventObject(date: birthDate, title: title);
 }
--- lib/models/event.g.dart
+++ lib/models/event.g.dart
@@ -6,29 +6,26 @@ part of 'event.dart';
 // TypeAdapterGenerator
 // **************************************************************************
 
-class EventAdapter extends TypeAdapter<Event> {
+class EventObjectAdapter extends TypeAdapter<EventObject> {
   @override
   final int typeId = 1;
 
   @override
-  Event read(BinaryReader reader) {
+  EventObject read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return Event(
-      id: fields[0] as int,
+    return EventObject(
       title: fields[1] as String,
       date: fields[2] as DateTime,
     );
   }
 
   @override
-  void write(BinaryWriter writer, Event obj) {
+  void write(BinaryWriter writer, EventObject obj) {
     writer
-      ..writeByte(3)
-      ..writeByte(0)
-      ..write(obj.id)
+      ..writeByte(2)
       ..writeByte(1)
       ..write(obj.title)
       ..writeByte(2)
@@ -41,7 +38,7 @@ class EventAdapter extends TypeAdapter<Event> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is EventAdapter &&
+      other is EventObjectAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
